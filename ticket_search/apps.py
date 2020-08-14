20fbdb567caf22c168eff7179a36630e4cf9c5f9
Leo Kukharau <kuhorev.leonid@gmail.com>
2020-08-14 12:10:40 -0700

    qcluster added on startup

--- ticket_search/apps.py
+++ ticket_search/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class TicketSearchConfig(AppConfig):
     name = 'ticket_search'
+
--- users/apps.py
+++ users/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
+from django.core.management import call_command
 
 
 class UsersConfig(AppConfig):
     name = 'users'
+
+    def ready(self):
+        call_command('qcluster')
