ae09e4d247b6f43c97f12ae646667210b1e90ab5
Leo Kukharau <kuhorev.leonid@gmail.com>
2020-08-14 12:48:01 -0700

    autostart command removed

--- users/apps.py
+++ users/apps.py
@@ -1,9 +1,5 @@
 from django.apps import AppConfig
-from django.core.management import call_command
 
 
 class UsersConfig(AppConfig):
     name = 'users'
-
-    def ready(self):
-        call_command('qcluster')
